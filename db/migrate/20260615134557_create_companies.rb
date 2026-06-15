36db69d506372f2381bb86ada888d403ee63e218
シーズンマサ・ソース <se.ason.masa@gmail.com>
2026-06-15 22:50:44 +0900

    Add Company management

---
+++ db/migrate/20260615134557_create_companies.rb
@@ -0,0 +1,11 @@
+class CreateCompanies < ActiveRecord::Migration[8.1]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :contact_person
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_15_130845) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_15_134557) do
+  create_table "companies", force: :cascade do |t|
+    t.string "contact_person"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "phone"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sites", force: :cascade do |t|
     t.string "address"
     t.datetime "created_at", null: false
